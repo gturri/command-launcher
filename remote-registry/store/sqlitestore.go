59095665d3a21cad029be194965882f9c27cbb63
Guillaume Turri <g.turri@criteo.com>
2025-10-24 19:03:25 +0200

    Simplify schema creation

--- remote-registry/store/sqlitestore.go
+++ remote-registry/store/sqlitestore.go
@@ -43,38 +43,20 @@ func (s *SqliteStore) createTablesIfNotExists() error {
 			name TEXT UNIQUE NOT NULL,
 			description TEXT
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS registry_admins (
 			registry_id INTEGER,
 			admin TEXT,
 			FOREIGN KEY (registry_id) REFERENCES registries(id) ON DELETE CASCADE
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS registry_custom_values (
 			registry_id INTEGER,
 			key TEXT,
 			value TEXT,
 			FOREIGN KEY (registry_id) REFERENCES registries(id) ON DELETE CASCADE
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS packages (
 			id INTEGER PRIMARY KEY,
 			name TEXT UNIQUE NOT NULL,
@@ -82,26 +64,14 @@ func (s *SqliteStore) createTablesIfNotExists() error {
 			registry_id INTEGER,
 			FOREIGN KEY (registry_id) REFERENCES registries(id) ON DELETE CASCADE
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS package_custom_values (
 			package_id INTEGER,
 			key TEXT,
 			value TEXT,
 			FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS package_versions (
 			package_id INTEGER,
 			version TEXT,
@@ -111,38 +81,20 @@ func (s *SqliteStore) createTablesIfNotExists() error {
 			end_partition INT,
 			FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS package_admins (
 			package_id INTEGER,
 			admin TEXT,
 			FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS deleted_packages (
 			registry_id INTEGER,
 			package_name TEXT,
 			FOREIGN KEY (registry_id) REFERENCES registries(id) ON DELETE CASCADE
 			PRIMARY KEY (registry_id, package_name)
 		);
-	`)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
 
-	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS deleted_registries (
 			registry_name TEXT,
 			PRIMARY KEY (registry_name)
